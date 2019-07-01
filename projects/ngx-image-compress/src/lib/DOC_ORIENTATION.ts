620a7a5c7a57c01bdaa4e3d6c371e53fd14f18b3
Gethin Oakes <gethinoakes@gmail.com>
2019-07-01 14:58:45 +0100

    Create DOC_ORIENTATION.ts

---
+++ projects/ngx-image-compress/src/lib/DOC_ORIENTATION.ts
@@ -0,0 +1,12 @@
+export enum DOC_ORIENTATION {
+  Up = 1,
+  Down = 3,
+  Right = 6,
+  Left = 8,
+  UpMirrored = 2,
+  DownMirrored = 4,
+  LeftMirrored = 5,
+  RightMirrored = 7,
+  NotJpeg = -1,
+  NotDefined = -2
+}
