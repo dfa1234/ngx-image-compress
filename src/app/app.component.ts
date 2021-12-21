680e5c9995b92ae9ab7bef49a7b5cb0b84806242
dfa1234 <dfaure@outlook.com>
2021-12-21 02:43:20 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import {Component} from '@angular/core';
-import {UploadResponse, DataUrl, NgxImageCompressService} from 'ngx-image-compress';
-import {DOC_ORIENTATION} from '../../projects/ngx-image-compress/src/lib/models/DOC_ORIENTATION';
+import { Component } from '@angular/core';
+import {
+  UploadResponse,
+  DataUrl,
+  NgxImageCompressService,
+  DOC_ORIENTATION,
+} from 'ngx-image-compress';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-  constructor(private imageCompress: NgxImageCompressService) {
-  }
+  constructor(private imageCompress: NgxImageCompressService) {}
 
   imgResultBeforeCompress: DataUrl = '';
   imgResultAfterCompress: DataUrl = '';
@@ -19,22 +21,27 @@ export class AppComponent {
   imgResultMultiple: UploadResponse[] = [];
 
   compressFile() {
-    this.imageCompress.uploadFile().then(({image, orientation}:UploadResponse) => {
-      this.imgResultBeforeCompress = image;
-      console.warn('Size in bytes was:', this.imageCompress.byteCount(image));
-      this.imageCompress.compressFile(image, orientation, 50, 50).then(
-        result => {
-          this.imgResultAfterCompress = result;
-          console.warn('Size in bytes is now:', this.imageCompress.byteCount(result));
-        }
-      );
-    });
+    this.imageCompress
+      .uploadFile()
+      .then(({ image, orientation }: UploadResponse) => {
+        this.imgResultBeforeCompress = image;
+        console.warn('Size in bytes was:', this.imageCompress.byteCount(image));
+        this.imageCompress
+          .compressFile(image, orientation, 50, 50)
+          .then((result) => {
+            this.imgResultAfterCompress = result;
+            console.warn(
+              'Size in bytes is now:',
+              this.imageCompress.byteCount(result)
+            );
+          });
+      });
   }
 
   uploadFile() {
-    this.imageCompress.uploadFile().then(({image, orientation}) => {
+    this.imageCompress.uploadFile().then(({ image, orientation }) => {
       this.imgResultUpload = image;
-      console.warn('DOC_ORIENTATION:',DOC_ORIENTATION[orientation]);
+      console.warn('DOC_ORIENTATION:', DOC_ORIENTATION[orientation]);
       console.warn(image);
     });
   }
@@ -44,5 +51,4 @@ export class AppComponent {
       this.imgResultMultiple = multipleOrientedFiles;
     });
   }
-
 }
